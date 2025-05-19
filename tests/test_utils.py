91bb7a4c984a0862cc7e17dfca64cccdca28e8f3
Peter Hviid Christiansen <repetepc@gmail.com>
2025-05-19 12:04:47 +0200

    Add example recipe

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,10 @@ from hafnia.utils import view_recipe_content
 
 
 def test_view_recipe_content():
-    tree_str = view_recipe_content(Path("./recipe.zip"))
+    # Recreates 'recipe.zip' from the `recipe-classification` example.
+    # Below command assumes that `recipe-classification` is available in same directory as the `hafnia` repo.
+    #       hafnia recipe create ../recipe-classification/ && mv recipe.zip tests/data/
+    tree_str = view_recipe_content(Path("tests/data/recipe.zip"))
 
     assert "train.py" in tree_str
     assert "Dockerfile" in tree_str
