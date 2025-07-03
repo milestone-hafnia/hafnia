51c59203fac2531f53d75f5cf27db0d9750320a4
Peter Christiansen <repetepc@gmail.com>
2025-07-03 08:55:48 +0000

    Change argument name for 'load_dataset'

--- src/hafnia/data/factory.py
+++ src/hafnia/data/factory.py
@@ -6,18 +6,18 @@ from hafnia import utils
 from hafnia.dataset.hafnia_dataset import HafniaDataset
 
 
-def load_dataset(data_recipe: Any, force_redownload: bool = False) -> HafniaDataset:
+def load_dataset(recipe: Any, force_redownload: bool = False) -> HafniaDataset:
     """Load a dataset either from a local path or from the Hafnia platform."""
 
-    path_dataset = get_dataset_path(data_recipe, force_redownload=force_redownload)
+    path_dataset = get_dataset_path(recipe, force_redownload=force_redownload)
     dataset = HafniaDataset.from_path(path_dataset)
     return dataset
 
 
-def get_dataset_path(data_recipe: Any, force_redownload: bool = False) -> Path:
+def get_dataset_path(recipe: Any, force_redownload: bool = False) -> Path:
     if utils.is_remote_job():
         return Path(os.getenv("MDI_DATASET_DIR", "/opt/ml/input/data/training"))
 
-    path_dataset = HafniaDataset.from_recipe_to_disk(data_recipe, force_redownload=force_redownload)
+    path_dataset = HafniaDataset.from_recipe_to_disk(recipe, force_redownload=force_redownload)
 
     return path_dataset
